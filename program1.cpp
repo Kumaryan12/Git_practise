55312f6c8fe56fe98443292673d45653e9aecb1d
Aryan <kumararyan66472@gmail.com>
2024-12-26 15:11:02 +0530

    Initial commit: Added program1.cpp  and program2.cpp

---
+++ program1.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World from program1.c!\n");
+    return 0;
+}
---
+++ program2.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World from program2.c!\n");
+    return 0;
+}
